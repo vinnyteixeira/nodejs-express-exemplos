b8c3139b39a41c482ef09b5156462b78a02fdcf3
Vinicius Teixeira <vinyteixeira.12@gmail.com>
2022-11-03 16:50:56 -0300

    Criado rotas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-const express = require('express')
+import * as express from 'express'
+
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('GET /')
+})
+
+app.post('/', (req, res) => {
+  res.status(201).send('POST /')
+})
+
+app.put('/', (req, res) => {
+  res.send('PUT /')
+})
+
+app.delete('/', (req, res) => {
+  res.send('DELETE /')
+})
+
+app.patch('/', (req, res) => {
+  res.send('PATCH /')
 })
 
 app.listen(3000, () => console.log(`ONLINE http://localhost:3000`))
