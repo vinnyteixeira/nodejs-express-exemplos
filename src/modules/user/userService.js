6c9a41621d34bf76c7b60e291fa8428eafde9a3c
Vinicius Teixeira <vinyteixeira.12@gmail.com>
2022-11-04 14:00:14 -0300

    Criado tratativa de erros para signup

--- src/modules/user/userService.js
+++ src/modules/user/userService.js
@@ -4,10 +4,8 @@ const getUserByEmail = (searchEmail) =>
  users.find(( obj ) => obj.email === searchEmail)
 
 export const signup = (data) => {
-    if (getUserByEmail(data.email)){
-        console.log("existe o email")
-    } else {
-        users.push(data)
-    }
+    if (getUserByEmail(data.email)) throw new Error ("email_existente")
+     
+    users.push(data)
     return true
 }
