8f95f149fad74831941b576ce94bb83a2c7b03e2
Vinicius Teixeira <vinyteixeira.12@gmail.com>
2022-11-04 15:25:15 -0300

    Criado service de post

--- src/modules/post/postController.js
+++ src/modules/post/postController.js
@@ -1,14 +1,29 @@
 import { Router } from "express";
 import { verifyAccessToken } from "../../utils/auth";
+import { createPost,getPosts } from "./postService";
 
 const router = Router();
 
 router.post('/', verifyAccessToken, (req, res) => {
-    res.send('CREATE POST /')
+    try {
+      const post = createPost(req.body, req.user)
+       res.status(200).send(post)
+    } catch (err) {
+      res.status(500).send(post)
+    }
   })
   
   router.get('/:id?', verifyAccessToken, (req, res) => {
-    res.send('GET POST /')
+    try {
+      const posts = getPosts(req.params.id)
+      res.status(200).send(posts)
+    } catch (err) {
+      if(err.message === 'post_nao_existe')
+      return res.status(400).send(err.message)
+
+      res.status(500).send(err.message)
+    }
+    
   })
   
   
---
+++ src/modules/post/postService.js
@@ -0,0 +1,20 @@
+let posts = []
+
+export const createPost = (data, user) => {
+    const post = {
+        createdBy: user.email,
+        ...data
+    }
+
+    posts.push(post)
+    return post
+}
+
+export const getPosts = (id) => {
+    if(id) {
+        const post = posts[id]
+        if (!post) throw new Error('post_nao_existe')
+        return post
+    }
+    return posts
+}
